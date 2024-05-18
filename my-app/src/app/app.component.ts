94ec5fbde7a321e2c7a9a0f0bb972d0f9e387b45
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2024-05-17 23:46:30 -0300

    alterando...

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
-import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PrimeiroComponenteComponent, SegundoComponenteComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
